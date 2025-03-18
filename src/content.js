017298bc6ab4c53419c804fb991ffd8208ba6099
G0ps <driveofgopi@gmail.com>
2025-03-18 15:56:22 +0530

    deleted unecessary file

--- src/content.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded" , () => runApp());
-
-async function runApp()
-{
-    chrome.runtime.sendMessage({background : ready} ,(response) => console.log(response.status)  );
-}
-  
